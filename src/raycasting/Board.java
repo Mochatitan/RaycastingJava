1b9c1b38cd739eb91cecebb6bb4b6a5a3f11755b
Mochatitan <119437575+Mochatitan@users.noreply.github.com>
2023-02-24 11:43:35 -0500

    Added basic attributes

---
+++ src/raycasting/Board.java
@@ -0,0 +1,174 @@
+package raycasting;
+
+public class Board{
+//Constants
+  //raycasting
+  public static final int FOV = 70;
+  
+  //visuals
+  public static final int MAP_HEIGHT = 24;
+  public static final int MAP_WIDTH = 24;
+  public static final int SCREEN_HEIGHT = 480;
+  public static final int SCREEN_WIDTH = 640;
+  
+  //game structure
+    //delay between each tick in ms
+    public static final int DELAY = 15;
+    // keep a reference to the timer object that triggers actionPerformed() 
+    private Timer timer;
+  
+public static final int worldMap[MAP_WIDTH][MAP_HEIGHT]=
+{
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+  
+public Board(){
+
+          // set the game board size
+        setPreferredSize(new Dimension(, TILE_SIZE * ROWS));
+        // set the game board background color
+        setBackground(new Color(232, 232, 232));
+
+        // initialize the game state
+        player = new Player();
+       
+
+        // this timer will call the actionPerformed() method every DELAY ms
+        timer = new Timer(DELAY, this);
+        timer.start();
+}
+  
+      @Override
+    public void actionPerformed(ActionEvent e) {
+        // this method is called by the timer every DELAY ms.
+        // use this space to update the state of your game or animation
+        // before the graphics are redrawn.
+
+        // prevent the player from disappearing off the board
+        player.tick();
+
+        // give the player points for collecting coins
+        //collectCoins();
+
+        // calling repaint() will trigger paintComponent() to run again,
+        // which will refresh/redraw the graphics.
+        repaint();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // when calling g.drawImage() we can use "this" for the ImageObserver 
+        // because Component implements the ImageObserver interface, and JPanel 
+        // extends from Component. So "this" Board instance, as a Component, can 
+        // react to imageUpdate() events triggered by g.drawImage()
+
+        // draw our graphics.
+        //drawBackground(g);
+        drawRaycast(g);
+        drawScore(g);
+        //for (Coin coin : coins) {
+           // coin.draw(g, this);
+        //}
+        //player.draw(g, this);
+
+        // this smooths out animations on some systems
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // this is not used but must be defined as part of the KeyListener interface
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // react to key down events
+        player.keyPressed(e);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // react to key up events
+    }
+
+    private void drawRaycast(Graphics g) {
+        // draw a checkered background
+        g.setColor(new Color(214, 214, 214));
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLUMNS; col++) {
+                
+               
+              
+                if(GAME_MAP[row][col]==1){
+                      g.fillRect(
+                        col * TILE_SIZE, 
+                        row * TILE_SIZE, 
+                        TILE_SIZE, 
+                        TILE_SIZE
+                      );
+                }
+                
+            }    
+        }
+    }
+
+    private void drawScore(Graphics g) {
+        // set the text to be displayed
+        String text = "$" + player.getScore();
+        // we need to cast the Graphics to Graphics2D to draw nicer text
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(
+            RenderingHints.KEY_TEXT_ANTIALIASING,
+            RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2d.setRenderingHint(
+            RenderingHints.KEY_RENDERING,
+            RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setRenderingHint(
+            RenderingHints.KEY_FRACTIONALMETRICS,
+            RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        // set the text color and font
+        g2d.setColor(new Color(30, 201, 139));
+        g2d.setFont(new Font("Lato", Font.BOLD, 25));
+        // draw the score in the bottom center of the screen
+        // https://stackoverflow.com/a/27740330/4655368
+        FontMetrics metrics = g2d.getFontMetrics(g2d.getFont());
+        // the text will be contained within this rectangle.
+        // here I've sized it to be the entire bottom row of board tiles
+        Rectangle rect = new Rectangle(0, TILE_SIZE * (ROWS - 1), TILE_SIZE * COLUMNS, TILE_SIZE);
+        // determine the x coordinate for the text
+        int x = rect.x + (rect.width - metrics.stringWidth(text)) / 2;
+        // determine the y coordinate for the text
+        // (note we add the ascent, as in java 2d 0 is top of the screen)
+        int y = rect.y + ((rect.height - metrics.getHeight()) / 2) + metrics.getAscent();
+        // draw the string
+        g2d.drawString(text, x, y);
+    }
+  
+  
+  
+  
+}
